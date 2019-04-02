eb71f5bcdc216cacba09e7782ec9259f2240db91
Emanuel Zgraggen <emanuel.zgraggen@gmail.com>
2019-04-02 14:12:03 -0400

    proper handling for shipping and pickup options in listing view

--- app/presenters/listing_presenter.rb
+++ app/presenters/listing_presenter.rb
@@ -104,15 +104,16 @@ class ListingPresenter < MemoisticPresenter
   end
 
   def delivery_config(require_shipping_address, pickup_enabled, shipping_price, shipping_price_additional, currency)
-    shipping = delivery_price_hash(:shipping, shipping_price, shipping_price_additional) if require_shipping_address
-    pickup = delivery_price_hash(:pickup, Money.new(0, currency), Money.new(0, currency))
-
     case [require_shipping_address, pickup_enabled]
     when matches([true, true])
+      shipping = delivery_price_hash(:shipping, shipping_price, shipping_price_additional, true) if require_shipping_address
+      pickup = delivery_price_hash(:pickup, Money.new(0, currency), Money.new(0, currency), false)
       [shipping, pickup]
     when matches([true, false])
+      shipping = delivery_price_hash(:shipping, shipping_price, shipping_price_additional, true) if require_shipping_address
       [shipping]
     when matches([false, true])
+      pickup = delivery_price_hash(:pickup, Money.new(0, currency), Money.new(0, currency), true)
       [pickup]
     else
       []
@@ -142,12 +143,12 @@ class ListingPresenter < MemoisticPresenter
     delivery_opts.present? ? delivery_opts.first[:shipping_price_additional] : nil
   end
 
-  def delivery_price_hash(delivery_type, price, shipping_price_additional)
+  def delivery_price_hash(delivery_type, price, shipping_price_additional, d)
     { name: delivery_type,
       price: price,
       shipping_price_additional: shipping_price_additional,
       price_info: ListingViewUtils.shipping_info(delivery_type, price, shipping_price_additional),
-      default: true
+      default: d
     }
   end
 
